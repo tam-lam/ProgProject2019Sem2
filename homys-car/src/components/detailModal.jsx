454b6d570d7d979361b6389bbdfccaa27f31459c
Tam Lam <imtamtlam@gmail.com>
2019-04-06 21:14:20 +1100

    react-bootstrap modal

---
+++ homys-car/src/components/detailModal.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Modal } from "react-bootstrap/Modal";
+class DetailModal extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Modal />
+      </React.Fragment>
+    );
+  }
+}
+
+export default DetailModal;
