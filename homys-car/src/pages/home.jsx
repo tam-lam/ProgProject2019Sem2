07d7ef48db2728b447d3c81fdc09faf46b35156c
Tam Lam <imtamtlam@gmail.com>
2019-04-07 21:30:15 +1000

    button to test modal

--- homys-car/src/pages/home.jsx
+++ homys-car/src/pages/home.jsx
@@ -15,6 +15,7 @@ class Home extends Component {
           title="Rent now"
           subtitle='Homy&apos;s car is one of the easiest and fastest car rental service in the world. Simply click on "Get my location"  and select a vehicle near by and start renting '
         />
+        <div className="btn btn-primary">Test Modal</div>
         <div className="container-fluid bg-dark">
           <div className="row">
             <div className="col-lg-7 nopadding my-col order-lg-2 mapContainer">
