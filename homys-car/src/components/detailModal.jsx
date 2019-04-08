efa367999e873c0646a634655586259a824a6970
Tam Lam <imtamtlam@gmail.com>
2019-04-08 19:26:29 +1000

    detail modal code

--- homys-car/src/components/detailModal.jsx
+++ homys-car/src/components/detailModal.jsx
@@ -5,7 +5,36 @@ class DetailModal extends Component {
   render() {
     return (
       <React.Fragment>
-        <Modal />
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Body closeButton>
+            <h4>Centered Modal</h4>
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+              ac consectetur aac, vestibulum at eros.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.onHide} className="btn-dark">
+              Close
+            </Button>
+            <Button
+              onClick={() => this.props.history.push("/testRoute")}
+              className="btn-dark"
+            >
+              Rent Now
+            </Button>
+            <Link className="btn btn-primary" to="/testRoute">
+              {" "}
+              testRoute
+            </Link>
+          </Modal.Footer>
+        </Modal>
       </React.Fragment>
     );
   }
