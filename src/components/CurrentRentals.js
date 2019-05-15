8804bc26660384c0a3fc7c71c8bac9328401d331
Tam Lam <imtamtlam@gmail.com>
2019-05-15 14:24:36 +1000

    update

---
+++ src/components/CurrentRentals.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+class CurrentRentals extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="rounded shadow-lg" style={{ marginTop: 20 }}>
+          Current Rentals
+        </h1>
+
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+        <h4>2019/04/12</h4>
+        <p>rent: car tye: #3455667 AU$36/h</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CurrentRentals;
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,192 +1,85 @@
 import React, { Component } from "react";
 import NavBar from "../components/Navbar";
+import userImg from "../images/userP.jpg";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import UserAvatar from "../components/UserAvatar";
 import Footer from "../components/Footer";
-import ListGroup from 'react-bootstrap/ListGroup'
-import Card from 'react-bootstrap/Card'
-import ListGroupItem from 'react-bootstrap/ListGroupItem'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
-import userP from "../images/userP.jpg";
-import Tab from 'react-bootstrap/Tab'
-import Nav from 'react-bootstrap/Nav'
-import TabContent from 'react-bootstrap/TabContent'
-import TabPane from 'react-bootstrap/TabPane'
-import TabContainer from 'react-bootstrap/TabContainer'
-import Signin from './Signin'
-import Image from 'react-bootstrap/Image'
-import Row from 'react-bootstrap/Row'
-import Col  from 'react-bootstrap/Col'
-export default function User(){
+import UserInformation from "../components/UserInformation";
+import RentalHistory from "./RentalHistory";
+import CurrentRentals from "../components/CurrentRentals";
+import SimplePageTitle from "../components/SimplePageTitle";
 
-   return ( 
-<React.Fragment>
-<NavBar />
+class User extends Component {
+  state = {
+    userName: "Ten fwaklj",
+    id: 1234,
+    email: "Ten@gmail.com",
+    firstName: "Ten",
+    lastName: "Sfafafw",
+    license: "ACB22445579",
+    dateOfBirth: "01/01/2000"
+  };
+  render() {
+    const {
+      userName,
+      id,
+      email,
+      firstName,
+      lastName,
+      license,
+      dateOfBirth
+    } = this.state;
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="container main-container">
+          <div className="main">
+            <Tab.Container id="left-tabs-example" defaultActiveKey="userInfo">
+              <Row className=" text-light">
+                <Col md={3} className="text-light">
+                  <div className="nav-container shadow-lg bg-light text-dark">
+                    <Nav variant="pills" className="flex-column">
+                      <UserAvatar userImg={userImg} userName={userName} />
+                      <Nav.Link className="text-dark" eventKey="userInfo">
+                        User infomation
+                      </Nav.Link>
+                      <Nav.Link className="text-dark" eventKey="currentRentals">
+                        Current rentals
+                      </Nav.Link>
+                      <Nav.Link className="text-dark" eventKey="rentalHistory">
+                        Rental history
+                      </Nav.Link>
+                      <Nav.Link className="text-dark" eventKey="confirmation">
+                        Confirmation
+                      </Nav.Link>
+                    </Nav>
+                  </div>
+                </Col>
 
-<h1 className=" text-light text-center">User infomation view system</h1><div className="main">
-<Card className="bg-dark text-white">
-  
-  <Card.ImgOverlay>
- 
-  <Image src={userP}   height="90" width="100"roundedCircle />
-    <Card.Title>Ten</Card.Title>
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-  <Row className="bg-light text-dark">
-    <Col sm={3} className="bg-dark text-light">
-    
-      <Nav variant="pills" className="flex-column">
-        <Nav.Item>
-          <Nav.Link eventKey="first" >User infomation:</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="second">Payment details:</Nav.Link>
-        </Nav.Item>
-        
-      </Nav>
-    </Col>
+                <Col md={9}>
+                  <Tab.Content className="">
+                    <Tab.Pane eventKey="userInfo">
+                      <UserInformation />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="rentalHistory">
+                      <RentalHistory />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="currentRentals">
+                      <CurrentRentals />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </div>
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
-    <Col sm={9}>
-      <Tab.Content>
-        <Tab.Pane eventKey="first">
-        <h1><strong>User information</strong> </h1>
-
-        <div className="h"><hr></hr></div>
-          <h4><strong>User name:  </strong> </h4>
-          <p> TEN</p> <div className="r"><hr></hr></div>
-          <h4><strong>User ID:</strong> </h4>
-          <p>Ten</p> <div className="r"><hr></hr></div>
-          <h4><strong>User Email:</strong> </h4>
-          <p>Ten@gmail.com</p> <div className="r"><hr></hr></div>
-          <h4><strong>User license_no:</strong> </h4>
-          <p>ACB22445579</p> <div className="r"><hr></hr></div>
-          <h4><strong>Date of birth:</strong> </h4>
-          <p>01/01/2000</p> <div className="r"><hr></hr></div>
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-        <h1><strong>Payment details:</strong></h1>
-        <div className="h"><hr></hr></div>
-        <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/13</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/14</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/16</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/15</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p> 
-        </Tab.Pane>
-        <Tab.Pane eventKey="fifth">
-        <h3><strong>Payment details:</strong></h3>
-        <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-          <h4>2019/04/12</h4>
-          <p>
-            
-            rent: car tye: #3455667
-            AU$36/h
-            
-          </p>
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-          
-        </Tab.Pane>
-        <Tab.Pane eventKey="first">
-          
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-          
-        </Tab.Pane>
-      </Tab.Content>
-    </Col>
-  </Row>
-</Tab.Container>
-</Card.ImgOverlay>
-</Card>
-
-   
-
-
-
-</div>
-</React.Fragment>
-)
-};
+export default User;
